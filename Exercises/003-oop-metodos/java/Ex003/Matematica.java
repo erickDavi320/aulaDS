b751296a5cdc58bb0929fcad34c6dcba37dd9057
Allan Lima <allandelima@proton.me>
2023-04-04 16:05:19 -0300

    :bug: Ex003 Division By Zero - OOP 003

--- Exercises/003-oop-metodos/java/Ex003/Matematica.java
+++ Exercises/003-oop-metodos/java/Ex003/Matematica.java
@@ -1,8 +1,5 @@
-import java.util.Scanner;
 
 public class Matematica {
-	
-	Scanner in = new Scanner(System.in);
 
 	//declarando variaveis
 	public double valorUm;
@@ -36,8 +33,12 @@ public class Matematica {
 	}
 	
 	//Metodos com parametros e com retorno
-	public double fazerDivisao(double valorUm, double valorDois) {
+	public double fazerDivisao(Double valorUm, Double valorDois) {
 		
+		if (valorDois == null || valorDois == 0) { // Verificação se o valorDois é nulo ou zero
+			throw new IllegalArgumentException("Não é possível dividir por zero ou por valor nulo."); // Exceção lançada caso o valorDois seja nulo ou zero
+		}
+
 		resultado = valorUm / valorDois;
 		
 		return resultado;
